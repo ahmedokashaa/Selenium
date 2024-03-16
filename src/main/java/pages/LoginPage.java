4ee32f4657bd02aa5032feab7581f1d79a419194
ahmedokashaa <ahmedokasha3991@gmail.com>
2024-03-16 19:09:23 +0200

    commit

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage {
 
     WebDriver driver;
+    public LoginPage( WebDriver driver){
+        this.driver = driver;
+
+        
+    }
 
     //elements
     By usernameField = By.id("user-name");
